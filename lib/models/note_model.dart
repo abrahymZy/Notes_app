0576c1fc652c50108fd6ec557b02b5eb29a1f9fe
Your Name <you@example.com>
2025-08-10 17:21:33 +0300

    create note model

---
+++ lib/models/note_model.dart
@@ -0,0 +1,9 @@
+class NoteModel {
+  NoteModel({required this.title, required this.subTitle, required this.date, required this.color});
+  
+  final String title;
+  final String subTitle;
+  final String date;
+  final int color;
+
+}
