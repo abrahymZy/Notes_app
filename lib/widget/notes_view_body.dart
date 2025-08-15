eec370f84c595c8a4c91013292dcbf21af266db0
Your Name <you@example.com>
2025-08-15 20:03:35 +0300

    create fetchAllNotes method

--- lib/widget/notes_view_body.dart
+++ lib/widget/notes_view_body.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:notes_app/cubits/notes_cubit/notes_cubit.dart';
 import 'package:notes_app/widget/notes_list_view.dart';
 
 class NotesViewBody extends StatelessWidget {
@@ -6,6 +8,9 @@ class NotesViewBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return NotesListView();
+    return BlocProvider(
+      create: (context) => NotesCubit(),
+      child: NotesListView(),
+    );
   }
 }
