e7a78c25addc3c4a1a07352afb5c8b660a4db335
Your Name <you@example.com>
2025-08-15 19:50:04 +0300

    create notes cubit

--- lib/views/notes_view.dart
+++ lib/views/notes_view.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:notes_app/views/add_note_bottom_sheet.dart';
 import 'package:notes_app/widget/add_note_button.dart';
 import 'package:notes_app/widget/custem_app_bar.dart';
 import 'package:notes_app/widget/notes_view_body.dart';
