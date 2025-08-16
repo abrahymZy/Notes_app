8ad37f2e0a426ba8a808aa41710dd6cbaba5e09c
Your Name <you@example.com>
2025-08-16 08:11:45 +0300

    delete note done

--- lib/widget/note_item.dart
+++ lib/widget/note_item.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:notes_app/cubits/notes_cubit/notes_cubit.dart';
 import 'package:notes_app/models/note_model.dart';
 import 'package:notes_app/views/edit_note_view.dart';
 
@@ -38,7 +40,11 @@ class NoteItem extends StatelessWidget {
               ),
               trailing: IconButton(
                 padding: EdgeInsets.only(bottom: 32),
-                onPressed: () {},
+                onPressed: () {
+                  note.delete();
+
+                  BlocProvider.of<NotesCubit>(context).fetchAllNotes();
+                },
                 icon: Icon(
                   FontAwesomeIcons.trash,
                   color: Colors.black,
